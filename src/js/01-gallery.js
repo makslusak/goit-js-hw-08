b4178552892a06ba8d03e2e90a699ecf78b2a023
makslusak <106471649+makslusak@users.noreply.github.com>
2022-09-13 14:04:25 +0300

    11

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,31 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+addMarkup(galleryItems);
+
+function createMarkup(items) {
+  return items
+    .map(item => {
+      return `<li><a class="gallery__item gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+      title="${item.description}"
+    />
+  </a></li>`;
+    })
+    .join('');
+}
+
+function addMarkup(arr) {
+  galleryRef.insertAdjacentHTML('beforeend', createMarkup(arr));
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
+gallery.on('show.simplelightbox', function () {});
+console.log(gallery);
