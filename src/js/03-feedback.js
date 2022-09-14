1dc1fe40a63b15e3283a03904101833def27b1c3
makslusak <106471649+makslusak@users.noreply.github.com>
2022-09-14 16:01:26 +0300

    3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import Throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = form.querySelector('[name="email"]');
+const message = form.querySelector('[name="message"]');
+const localKey = 'feedback-form-state';
+
+form.addEventListener('input', Throttle(storageFormData, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  // const { email, message } = event.currentTarget.elements;
+  // вывести в консоль в1
+  //   console.log({ email: email.value, message: message.value });
+  // вывести в консоль в2
+  const savedData = JSON.parse(localStorage.getItem(localKey));
+  console.dir(savedData);
+  localStorage.removeItem(localKey);
+  event.currentTarget.reset();
+}
+
+function storageFormData(event) {
+  const formValue = { email: '', message: '' };
+  if (localStorage.getItem(localKey)) {
+    Object.assign(formValue, JSON.parse(localStorage.getItem(localKey)));
+  }
+  formValue[event.target.name] = event.target.value;
+  localStorage.setItem(localKey, JSON.stringify(formValue));
+}
