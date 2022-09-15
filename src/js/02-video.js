3caab6114a50d8655a4f2e1d354fc4209582f762
makslusak <106471649+makslusak@users.noreply.github.com>
2022-09-15 15:45:22 +0300

    Fix bug in 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,81 +1,3 @@
-// import Player from '@vimeo/player';
-// import Throttle from 'lodash.throttle';
-
-// const timeKey = videoplayer - current - time;
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-// window.addEventListener('load', newStart);
-
-// function durationToStorage({ seconds }) {
-//   localStorage.setItem(timeKey, seconds);
-//   console.log(seconds);
-// }
-
-// player.on('timeupdate', Throttle(durationToStorage, 1000));
-
-// function newStart() {
-//   if (!localStorage.getItem(timeKey)) {
-//     return;
-//   }
-//   const currentVideoTime = localStorage.getItem(timeKey);
-//   player
-//     .setCurrentTime(currentVideoTime)
-//     .then(() => player.play())
-
-//     .catch(function (error) {
-//       switch (error.name) {
-//         case 'RangeError':
-//           // the time was less than 0 or greater than the video’s duration
-//           break;
-
-//         default:
-//           // some other error occurred
-//           break;
-//       }
-//     });
-// }
-
-//===========================================================
-// import Player from '@vimeo/player';
-// import Throttle from 'lodash.throttle';
-
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-// // сделали переменную для ключа
-// const timeKey = 'videoplayer-current-time';
-// // создали функцию, с помощью деструктаризации вытащили секунды собьекта тайм апдейт(записали в локальное хранилище)
-// function durationSaveToStorage({ seconds }) {
-//   localStorage.setItem(timeKey, seconds);
-// }
-// // при перезагрузки страницы перезагружался плеер
-// window.addEventListener('load', newStart);
-// player.on('timeupdate', Throttle(durationSaveToStorage, 1000));
-// // функция которая ловит ивент лоад, если в локальном хранилище есть запись, достань их
-// function newStart() {
-//   if (!localStorage.getItem(timeKey)) {
-//     return;
-//   }
-//   const currentVideoTime = localStorage.getItem(timeKey);
-
-//   player
-//     .setCurrentTime(currentVideoTime)
-//     .then(() => {
-//       player.play();
-//       // seconds = the actual time that the player seeked to
-//     })
-//     .catch(function (error) {
-//       switch (error.name) {
-//         case 'RangeError':
-//           // the time was less than 0 or greater than the video’s duration
-//           break;
-
-//         default:
-//           // some other error occurred
-//           break;
-//       }
-//     });
-// }
-
 import Player from '@vimeo/player';
 import Throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
@@ -98,9 +20,7 @@ function newStart() {
 const currentVideoTime = localStorage.getItem(timekey);
 player
   .setCurrentTime(currentVideoTime)
-  .then(() => {
-    player.play();
-  })
+  .then(() => {})
   .catch(function (error) {
     switch (error.name) {
       case 'RangeError':
